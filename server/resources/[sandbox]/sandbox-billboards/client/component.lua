cb341aa7bf845870ad7bf2d6b8fb2f6d0f74d9a3
Elijah <128943965+notelijah05@users.noreply.github.com>
2025-11-04 14:58:12 -0500

    Update component.lua

--- server/resources/[sandbox]/sandbox-billboards/client/component.lua
+++ server/resources/[sandbox]/sandbox-billboards/client/component.lua
@@ -1,6 +1,16 @@
 AddEventHandler('onClientResourceStart', function(resource)
 	if resource == GetCurrentResourceName() then
-		Wait(10000)
+		Wait(1000)
+		-- print('testing biatch')
+		-- local dui = CreateBillboardDUI('https://i.imgur.com/Zlf40QZ.png', 1024, 512)
+		-- AddReplaceTexture('ch2_03b_cg2_03b_bb', 'ch2_03b_bb_lowdown', dui.dictionary, dui.texture)
+
+		-- Wait(10000)
+
+		-- print(dui.id)
+
+		-- ReleaseBillboardDUI(dui.id)
+		-- RemoveReplaceTexture('ch2_03b_cg2_03b_bb', 'ch2_03b_bb_lowdown')
 		StartUp()
 	end
 end)
